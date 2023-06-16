a0685c19d22ca192f886bc5a636ba9b9e63f702b
RuslanNig <44723887+RuslanNig@users.noreply.github.com>
2023-06-16 21:32:58 +0300

    src subfolder created

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NeuralNetworkLearn
 #test push comment
 #redacted 09 june 15:54
+#
+#create superframe 1
+#next step - create lines
+# should create separate files
--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 #test commit push python file
 print("hello git!")
+
+#save it in file mymodule.py
+def greeting(name):
+    print("Hello, " + name)
+
+import sys
+sys.path.append('/Users/Lyceum1557/Pictures/src/')
+#main file in dir 'C/Users/Lyceum1557/Pictures/src/'
+#mymodule file in subdir src
+import mymodule
+mymodule.greeting("Ruslan")
